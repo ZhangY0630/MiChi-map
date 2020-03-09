8ca635699c4c2dc713bde72d9ed5c184abe43b51
Alex Zhang <zhangchi0104@live.com>
2020-03-09 09:27:42 +0800

    Finished implementing crawler and done crawling

--- crawler/main.go
+++ crawler/main.go
@@ -1,45 +1,83 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"mgcrawler/structs"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
 
 	"github.com/levigross/grequests"
 )
 
-func main() {
-	rp := structs.RequestParams{
-		Page: 0,
-		Attributes: []string{
-			"_geoloc",
-			"city_name",
-			"country_name",
-			"cuisine_type",
-			"street",
-			"michelin_award",
-			"name",
-			"url",
-		},
-		NumbersPerPage: 1,
-	}
-	fmt.Println(string(rp.JSON("prod-restaurants-en")))
+const (
+	maxNumPerPage       int    = 1000
+	totalNumRestaurants int    = 13821
+	sleepDuration       int    = 1000
+	indexName           string = "prod-restaurants-en"
+)
 
+var (
+	params = map[string]string{
+		"x-algolia-agent":          "Algolia for JavaScript (3.35.1); Browser (lite); instantsearch.js (4.1.1); JS Helper (3.0.0)",
+		"x-algolia-application-id": "8NVHRD7ONV",
+		"x-algolia-api-key":        "71b3cff102a474b924dfcb9897cc6fa8",
+	}
+	queryAttributes = []string{
+		"_geoloc",
+		"city_name",
+		"country_name",
+		"cuisine_type",
+		"street",
+		"michelin_award",
+		"name",
+	}
+)
+
+func main() {
+	_, es := os.Stat("../data/")
+	if es != nil && os.IsNotExist(es) {
+		os.MkdirAll("../data", os.ModePerm)
+	}
+	wg := &sync.WaitGroup{}
+	for i := 0; i < totalNumRestaurants/maxNumPerPage+1; i++ {
+		wg.Add(1)
+		go crawl(i, wg)
+		time.Sleep(time.Second * 1)
+	}
+	wg.Wait()
+}
+
+func crawl(pageIndex int, wg *sync.WaitGroup) {
+
+	fmt.Println("[LOG] Start crawling page", pageIndex)
+	rp := structs.RequestParams{
+		Page:           pageIndex,
+		Attributes:     queryAttributes,
+		NumbersPerPage: maxNumPerPage,
+	}
 	ro := &grequests.RequestOptions{
-		Params: map[string]string{
-			"x-algolia-agent":          "Algolia for JavaScript (3.35.1); Browser (lite); instantsearch.js (4.1.1); JS Helper (3.0.0)",
-			"x-algolia-application-id": "8NVHRD7ONV",
-			"x-algolia-api-key":        "71b3cff102a474b924dfcb9897cc6fa8",
-		},
-		JSON: rp.JSON("prod-restaurants-en"),
+		Params: params,
+		JSON:   rp.JSON(indexName),
 	}
 	resp, err := grequests.Post("https://8nvhrd7onv-2.algolianet.com/1/indexes/*/queries", ro)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(resp.String())
-		resp.DownloadToFile("page1.json")
-		res := structs.RespResult{}
-		resp.JSON(&res)
-		fmt.Println(len(res.Restaurants))
+		return
 	}
+
+	res := structs.ResponseData{}
+	resp.JSON(&res)
+	bytes, errJSON := json.Marshal(res.Results[0])
+	if errJSON != nil {
+		fmt.Printf("[ERROR] %s", errJSON.Error())
+	} else {
+		filename := filepath.Join("../data", fmt.Sprintf("data%d.json", pageIndex))
+		ioutil.WriteFile(filename, bytes, 0644)
+	}
+	fmt.Println("[LOG]Finished crawling page", pageIndex)
+	wg.Done()
 }
--- crawler/structs/response.go
+++ crawler/structs/response.go
@@ -1,15 +1,89 @@
 package structs
 
-// RestaurantInfo holds the fields that is required for crawled data
-type RestaurantInfo struct {
-	CityName      string `json:"city_name"`
-	CountryName   string `json:"country_name"`
-	CuisineType   string `json:"cuisine_type"`
-	MichelinAward string `json:"michelin_award"`
-	Street        string `json:"street"`
-}
-
-// RespResult is used for holding the original request
-type RespResult struct {
-	Restaurants []RestaurantInfo `json:"hits"`
-}
+// ResponseData stores the data in from response
+type ResponseData struct {
+	Results []struct {
+		ExhaustiveNbHits bool `json:"exhaustiveNbHits"`
+		Hits             []struct {
+			Geoloc struct {
+				Lat float64 `json:"lat"`
+				Lng float64 `json:"lng"`
+			} `json:"_geoloc"`
+			// HighlightResult struct {
+			// 	Geoloc struct {
+			// 		Lat struct {
+			// 			MatchLevel   string        `json:"matchLevel"`
+			// 			MatchedWords []interface{} `json:"matchedWords"`
+			// 			Value        string        `json:"value"`
+			// 		} `json:"lat"`
+			// 		Lng struct {
+			// 			MatchLevel   string        `json:"matchLevel"`
+			// 			MatchedWords []interface{} `json:"matchedWords"`
+			// 			Value        string        `json:"value"`
+			// 		} `json:"lng"`
+			// 	} `json:"_geoloc"`
+			// 	AreaName struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"area_name"`
+			// 	CityName struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"city_name"`
+			// 	CountryName struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"country_name"`
+			// 	CuisineType struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"cuisine_type"`
+			// 	MichelinAward struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"michelin_award"`
+			// 	Name struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"name"`
+			// 	Postcode struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"postcode"`
+			// 	RegionName struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"region_name"`
+			// 	Street struct {
+			// 		MatchLevel   string        `json:"matchLevel"`
+			// 		MatchedWords []interface{} `json:"matchedWords"`
+			// 		Value        string        `json:"value"`
+			// 	} `json:"street"`
+			// } `json:"_highlightResult"`
+			CityName      string `json:"city_name"`
+			CountryName   string `json:"country_name"`
+			CuisineType   string `json:"cuisine_type"`
+			MichelinAward string `json:"michelin_award"`
+			Name          string `json:"name"`
+			// ObjectID      string `json:"objectID"`
+			Street        string `json:"street"`
+		} `json:"hits"`
+		// HitsPerPage       float64 `json:"hitsPerPage"`
+		// Index             string  `json:"index"`
+		// NbHits            float64 `json:"nbHits"`
+		// NbPages           float64 `json:"nbPages"`
+		// Page              float64 `json:"page"`
+		// Params            string  `json:"params"`
+		// ProcessingTimeMS  float64 `json:"processingTimeMS"`
+		// Query             string  `json:"query"`
+		// QueryAfterRemoval string  `json:"queryAfterRemoval"`
+	}`json:"results"`
+} 
